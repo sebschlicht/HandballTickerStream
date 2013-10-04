4f75be8c7561828cfd770702b63e1fe976b29dac
sebschlicht <sebschlicht@uni-koblenz.de>
2013-10-04 14:08:16 +0200

    match phase test added

---
+++ src/test/java/de/jablab/HandballTickerStream/matchdata/MatchPhaseTest.java
@@ -0,0 +1,58 @@
+package de.jablab.HandballTickerStream.matchdata;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class MatchPhaseTest {
+
+	public static final String INVALID_MATCH_PHASE = "nophase";
+
+	private MatchPhase phase;
+
+	public void loadMatchPhase(final MatchPhase phase) {
+		this.phase = MatchPhase.parseString(phase.toString());
+		assertNotNull(this.phase);
+		assertEquals(phase, this.phase);
+	}
+
+	@Test
+	public void testPhaseInvalid() {
+		this.phase = MatchPhase.parseString(INVALID_MATCH_PHASE);
+
+		assertNull(this.phase);
+	}
+
+	@Test
+	public void testWarmup() {
+		this.loadMatchPhase(MatchPhase.WARMUP);
+	}
+
+	@Test
+	public void testFirst() {
+		this.loadMatchPhase(MatchPhase.FIRST);
+	}
+
+	@Test
+	public void testPaused() {
+		this.loadMatchPhase(MatchPhase.PAUSED);
+	}
+
+	@Test
+	public void testHalfTime() {
+		this.loadMatchPhase(MatchPhase.HALF_TIME);
+	}
+
+	@Test
+	public void testSecond() {
+		this.loadMatchPhase(MatchPhase.SECOND);
+	}
+
+	@Test
+	public void testFinished() {
+		this.loadMatchPhase(MatchPhase.FINISHED);
+	}
+
+}
